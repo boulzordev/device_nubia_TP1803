0c9d72def31365906772b478a8a2ae9d4616975b
Kuba Wojciechowski <nullbytepl@gmail.com>
2020-11-30 23:51:43 +0300

    TP1803: add a rro to disable telephony injection
    doesn't work, doesn't break anything, why care?
    
    Signed-off-by: Kuba Wojciechowski <nullbytepl@gmail.com>

--- device.mk
+++ device.mk
@@ -73,7 +73,8 @@ PRODUCT_PACKAGES += \
     qti-telephony-utils \
     qti_telephony_utils.xml \
     qti-telephony-hidl-wrapper \
-    qti_telephony_hidl_wrapper.xml
+    qti_telephony_hidl_wrapper.xml \
+    TelephonyResNubia
 
 # TextClassifier
 PRODUCT_PACKAGES += \
